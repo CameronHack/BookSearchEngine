e74c94a4cedc81d99a6699325b60096ed42da1a7
Cam <camhack67@gmail.com>
2023-09-28 11:40:44 -0400

    server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-
 const httpLink = createHttpLink({
   uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
 const client = new ApolloClient({
