7b1e7d1a21a39fd7ce6711846998fc5c10ff75e9
Cam <camhack67@gmail.com>
2023-09-29 14:05:26 -0400

    added user login front end

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,11 +2,13 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [loginUser, { error, data }] = useMutation(LOGIN_USER);
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
@@ -26,15 +28,14 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await loginUser({
+        variables: {
+          email: userFormData.email,
+          password: userFormData.password
+        },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(response);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,3 +12,15 @@ mutation createUser($username: String!, $email: String!, $password: String!) {
   }
 }
 `;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
